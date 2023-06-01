22dd6236944655279ca4cd0fdb038afa6b408d3d
Gilbert Sinnott <gilbert.sinnott@gmail.com>
2023-06-01 12:45:14 +0200

    amend init

--- initialFreshInstall.sh
+++ initialFreshInstall.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
 echo "------------- UPDATE -------------"
-
 sudo apt update
 
 echo "------------- SAMBA -------------"
-
 yes | sudo apt install samba
 
 echo "------------- GIT -------------"
-
 yes | sudo apt install git
 
 echo "------------- V4L2-UTILS -------------"
-
 yes | sudo apt install v4l-utils
 
 echo "------------- CURL -------------"
-
-yes | sudo apt install  curl
+yes | sudo apt install curl
 
 echo "------------- ZSH -------------"
-
 yes | sudo apt install zsh
 
 echo "------------- V4L2-LOOPBACK -------------"
+yes | sudo apt install build-essential
 
-yes | sudo apt install build-essential media-build
-yes | sudo apt v4l2loopback-dkms
+echo "------------- V4L2-LOOPBACK -------------"
+yes | sudo apt install v4l2loopback
 
-echo "------------- OH-MY-ZSH -------------"
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
+if [[ -z "$ZSH" ]]; then
+    echo "------------- OH-MY-ZSH -------------"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else
+    echo "Oh-My-Zsh is already installed."
+fi
