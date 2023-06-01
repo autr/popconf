659ca9fb127d268c7d78e51aa5e9b222da29ef29
Gilbert Sinnott <gilbert.sinnott@gmail.com>
2023-06-01 12:43:04 +0200

    amend init

--- initialFreshInstall.sh
+++ initialFreshInstall.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
-# Update package lists
+echo "------------- UPDATE -------------"
+
 sudo apt update
 
-# Install Samba
+echo "------------- SAMBA -------------"
+
 yes | sudo apt install samba
 
-# Install Git
+echo "------------- GIT -------------"
+
 yes | sudo apt install git
 
-# Install v4l2-ctl
+echo "------------- V4L2-UTILS -------------"
+
 yes | sudo apt install v4l-utils
+
+echo "------------- CURL -------------"
 
 yes | sudo apt install  curl
 
+echo "------------- ZSH -------------"
+
 yes | sudo apt install zsh
+
+echo "------------- V4L2-LOOPBACK -------------"
+
+yes | sudo apt install build-essential media-build
+yes | sudo apt v4l2loopback-dkms
+
+echo "------------- OH-MY-ZSH -------------"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
